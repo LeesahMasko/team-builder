f8a27d133e90d707a19f77181d50051872250d8f
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-03-24 14:54:40 -0700

    Project is set up, steps are being implemented

---
+++ team-builder/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+     <form>
+       <Form>
+
+       </Form>
+      
+
+
+     </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Form(props) {
+
+
+return (
+
+    <div>
+         {/* <input>
+        name
+        email
+        role
+       
+       </input> */}
+        Hellooo
+    </div>
+)
+
+}
+
+
+export default Form;
