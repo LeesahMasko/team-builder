3ba21d0913c2900e39687e931456fe23d1c88095
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-03-24 15:34:17 -0700

    Form seems to be showing, but it is not functional

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 import Form from "./Form";
 
+import "./App.css";
+
 function App() {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    role: ""
+  });
+  const onInputChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+    console.log(formData);
+  };
+  const [teamList] = useState([]);
+
+  const submitForm = event => {
+    teamList.push(formData);
+    setFormData({
+      fname: "",
+      lname: "",
+      email: "",
+      role: ""
+    });
+
+    event.preventDefault();
+
+    console.log(teamList);
+    console.log(formData);
+  };
+
   return (
     <div className="App">
-     <form>
-       <Form>
-
-       </Form>
-      
-
-
-     </form>
+      <header className="App-header">
+        <Form
+          onInputChange={onInputChange}
+          formData={formData}
+          submitForm={submitForm}
+        />
+      </header>
+      <h3>Team List</h3>
+     
     </div>
   );
 }
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React from "react";
 
-function Form(props) {
-
-
-return (
-
-    <div>
-         {/* <input>
-        name
-        email
-        role
-       
-       </input> */}
-        Hellooo
-    </div>
-)
-
-}
-
+const Form = props => {
+  return (
+    <>
+      <form onSubmit={props.submitForm}>
+        <h3>Add Member</h3>
+        <label htmlFor="fName"> Name: </label>
+        <input
+          onChange={props.onInputChange}
+          id="fName"
+          name="fname"
+          type="text"
+          value={props.formData.fname}
+        />
+        <label htmlFor="lName">Last Name: </label>
+        <input
+          onChange={props.onInputChange}
+          id="lName"
+          name="lname"
+          type="text"
+          value={props.formData.lname}
+        />
+        <label htmlFor="emailInput">Email: </label>
+        <input
+          onChange={props.onInputChange}
+          id="emailInput"
+          name="email"
+          type="text"
+          value={props.formData.email}
+        />
+        <label htmlFor="roleInput">Role: </label>
+        <input
+          onChange={props.onInputChange}
+          id="roleInput"
+          name="role"
+          type="text"
+          value={props.formData.role}
+        />
+        <button>Add</button>
+      </form>
+    </>
+  );
+};
 
 export default Form;
